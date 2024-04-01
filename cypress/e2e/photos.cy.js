eb34ab4fee9e5c7f67af1bf06f235423446d1943
quyenducngo <quyenducngo@gmail.com>
2024-04-01 15:43:04 -0700

    add test for photos page

--- cypress/e2e/photos.cy.js
+++ cypress/e2e/photos.cy.js
@@ -1,5 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://www.quyenducngo.com/')
+    cy.visit('https://www.quyenducngo.com/personal.html')
+    cy.get('h1').should('contain.text', 'Under Construction');
   })
 })
